96232bd06b24e623a4feb887afb7cd825fb19f50
Jhon-crypt <johnoladele690@gmail.com>
2022-09-23 01:47:38 -0700

    Change dependencies to CDN

--- index.php
+++ index.php
@@ -8,23 +8,25 @@
     <meta name="generator" content="Hugo 0.84.0">
     <title>Recap</title>
 
-    <!-- Bootstrap core CSS -->
-    <link href="./src/assets/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="./src/assets/font-awesome/css/font-awesome.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-    <link rel="stylesheet" href="./src/assets/angular-loading-bar-master/angular-loading-bar-master/build/loading-bar.min.css" 
-    type='text/css' media='all'>
+    <link rel='stylesheet' href='//cdnjs.cloudflare.com/ajax/libs/angular-loading-bar/0.9.0/loading-bar.min.css' type='text/css' 
+    media='all' />
 
     <!-- JavaScript Bundle with Popper -->
-    <script src="./src/assets/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
 
     <!-- angular -->
-    <script src="./src/assets/angular-1.8.2/angular.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.8.2/angular.min.js"></script>
 
-    <script src="./src/assets/angular-1.8.2/angular-route.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.8.2/angular-route.min.js"></script>
 
-    <script src="./src/assets/angular-loading-bar-master/angular-loading-bar-master/build/loading-bar.min.js"></script>
+    <script type='text/javascript' src='//cdnjs.cloudflare.com/ajax/libs/angular-loading-bar/0.9.0/loading-bar.min.js'></script>
 
   </head>
 
